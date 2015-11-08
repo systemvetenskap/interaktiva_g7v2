739d05180359503ae6f63ad1fa88363e21b9dc28
Michael Byrskog <micke884@hotmail.com>
2015-11-08 23:40:30 +0100

    cmit

--- WebApplication1/WebApplication1/JavaScript.js
+++ WebApplication1/WebApplication1/JavaScript.js
@@ -1,36 +1,4 @@
-﻿//var run = <%=this.timerVar %>;
-//if (run = 1)
-//{
-//    function timer() {
-//        var minutes = 30;
-//        var seconds = 00;ss
-//        var counter = setInterval(timer, 1000) //körs varje sekund
-
-//        seconds = seconds - 1
-//        if (seconds <= 0) {
-//            minutes -= 1;
-//            seconds += 59;
-//        }
-//        if (minutes <= 1) {
-//            minutes == 1;
-//            seconds = 0;
-//            clearInterval(counter);
-//            alert("Tiden är slut");
-//            //Kod här när tiden är slut
-//            return;
-//        }
-//        document.getElementById("timer").innerHTML = minutes + " minuter " + seconds + " sekunder";§
-//    }
-
-
-
-//}
-
-
-
-
-
-//Timern följer med när man scrollar ner
+﻿//Timern följer med när man scrollar ner
 $(document).ready(function () {
     var top = $('#timerbox').offset().top - parseFloat($('#timerbox').css('marginTop').replace(/auto/, 0));
     $(window).scroll(function (event) {
