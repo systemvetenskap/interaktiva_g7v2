9acb9471312e4bd8eba2d2e6b9826138c5d2c87e
hnkrpnk <hkauhanen@live.se>
2015-11-09 02:41:54 +0100

    cc

--- WebApplication1/WebApplication1/mytests.aspx.cs
+++ WebApplication1/WebApplication1/mytests.aspx.cs
@@ -9,6 +9,7 @@ using System.Data;
 using System.IO;
 using System.Web.Configuration;
 
+
 namespace WebApplication1
 {
     public partial class mytests : System.Web.UI.Page
@@ -276,69 +277,6 @@ namespace WebApplication1
                 conn.Close();
             }
         }
-
-        
-    }
-}
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Data;
-using Npgsql;
-
-namespace WebApplication1
-{
-    public partial class mytests : System.Web.UI.Page
-    {
-        NpgsqlConnection conn = new NpgsqlConnection("Server=webblabb.miun.se;Port=5432; User Id=pgmvaru_g7;Password=akrobatik;Database=pgmvaru_g7;SSL=true;");
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            DataTable dt = new DataTable();
-            DataTable dt2 = new DataTable();
-            string sql = @"select date, grade, points, name, leader.firstname, leader.lastname from license_test
-                            inner join users on license_test.user_id = users.userid
-                            inner join leader on users.leader_id = leader.leader_id
-                            where users.userid = 1";
-            conn.Open();
-
-            sql += "order by date desc";
-
-            NpgsqlCommand cmd = new NpgsqlCommand(sql, conn);
-            NpgsqlDataAdapter da = new NpgsqlDataAdapter(cmd);
-            da.Fill(dt);
-            dt2.Columns.Add("date");
-            dt2.Columns.Add("grade");
-            dt2.Columns.Add("points");
-            dt2.Columns.Add("name");
-            dt2.Columns.Add("leader");
-            string format = "yyyy-MM-dd";
-          
-
-                     // Use current time.
-           // Use this format.
-        
-
-            foreach (DataRow r in dt.Rows)
-            {
-                DataRow row = dt2.NewRow();
-                DateTime date = Convert.ToDateTime(r[0]);
-                row[0] = date.ToString(format);
-                row[1] = r[1];
-                row[2] = r[2];
-                row[3] = r[3];
-                row[4] = r[4].ToString() + " " + r[5].ToString();
-                dt2.Rows.Add(row);
-            }
-
-            GridViewTests.DataSource = dt2;
-            GridViewTests.DataBind();
-            conn.Close();
-
-        }
-
         protected void btnLicenseTest_Click(object sender, EventArgs e)
         {
             Application["type"] = "a";
@@ -351,5 +289,8 @@ namespace WebApplication1
             Application["type"] = "b";
             Response.Redirect("dotest.aspx");
         }
+
+
     }
 }
+
